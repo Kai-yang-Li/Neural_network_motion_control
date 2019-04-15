23dadbb11f3c5bf57239b73b5b5ccf685419fc5a
likaiyang <17649830270@163.com>
2019-04-15 23:35:39 +0800

    softmax:

--- Neural_network_motion_control/softmax_t.py
+++ Neural_network_motion_control/softmax_t.py
@@ -4,7 +4,7 @@ from mxnet import autograd, nd
 batch_size = 256
 tranin_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
-num_inputs = 784
+num_inputs = (28*28)
 num_outputs = 10
 
 w = nd.random.normal(scale=0.01, shape=(num_inputs, num_outputs))
@@ -12,3 +12,17 @@ b = nd.zeros(num_outputs)
 
 w.attach_grad()
 b.attach_grad()
+
+def softmax(x):
+    x_exp = x.exp()
+    partition = x_exp.sum(axis =1, keepdims = True)
+    return x_exp/partition
+
+def net(x):
+    return softmax( nd.dot(x.reshape((-1, num_inputs)),w)+b )
+
+def cross_entropy(y_hat, y):
+    return -nd.pick(y_hat, y).log()
+
+
+
